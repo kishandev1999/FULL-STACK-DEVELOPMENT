73bc36937414dba73ceab8dc0fea77d67d8557b1
Pithani.Kishan Dev <44198442+kishandev1999@users.noreply.github.com>
2020-11-24 23:07:57 +0530

    Add files via upload
    its js arrays and commenting

---
+++ array_and_loops.js
@@ -0,0 +1,36 @@
+// var students =["dev","kishan","asteick","devika","parnika","harika","renuka","hansika","geethika"];
+// var naughty =[];
+// naughty.push(students[0]);
+// console.log(naughty);
+// console.log(naughty.indexOf("dev"));
+// var index =students.indexOf(3);
+// if(index > -1){
+// 	students.splice(index,2);
+// }
+// console.log(students);
+
+var stu=["dev","kishan","asteick","devika","parnika","harika","renuka","hansika","geethika"];
+var nau =[];
+nau.push(stu[5]);
+// console.log(nau);
+var index = nau.indexOf("harika");
+console.log(nau);
+if(index > -1){
+	stu.splice(index,5);
+	console.log(stu);
+}
+
+//loops:
+var someval = 9;
+for(var x = 1; x<= someval; x++){
+	console.log(x);
+}
+console.log("rap");
+console.log(x);
+
+//loop lists:
+var list =["dev","kishan","krish","kiran","kangana"];
+for(var index = 0; index <= list.length; index++){
+	console.log(list[index]);
+}
+
---
+++ comments_nd_linking.js
@@ -0,0 +1,5 @@
+var name = "jack";
+var age = 23;
+
+var message = "Hi, My name is " + name + " and My age" + age + " years old";
+alert(message);
